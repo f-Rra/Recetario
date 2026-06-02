eaedb1829b3b7968b768025811eb828c54140686
f-Rra <facundo.rra@gmail.com>
2026-06-02 17:22:29 -0300

    feat(vistas): actualizar vw_ModificacionesRegistradas
    - Reemplazar m.Tipo por JOIN a TiposModificacion para obtener el nombre

--- scripts/Vistas/vw_ModificacionesRegistradas.sql
+++ scripts/Vistas/vw_ModificacionesRegistradas.sql
@@ -2,14 +2,15 @@ CREATE OR ALTER VIEW vw_ModificacionesRegistradas AS
 SELECT
     c.Fecha,
     r.Nombre           AS NombreReceta,
-    m.Tipo,
+    tm.Nombre          AS Tipo,
     iOrig.Descripcion  AS IngredienteOriginal,
     iRemp.Descripcion  AS IngredienteReemplazo,
     m.Cantidad,
     u.Abreviatura      AS Unidad
 FROM Modificaciones m
-    INNER JOIN Comandas    c     ON c.IdComanda          = m.IdComanda
-    INNER JOIN Recetas     r     ON r.IdReceta           = c.IdReceta
-    LEFT  JOIN Ingredientes iOrig ON iOrig.IdIngrediente = m.IdIngredienteOriginal
-    LEFT  JOIN Ingredientes iRemp ON iRemp.IdIngrediente = m.IdIngredienteReemplazo
-    INNER JOIN Unidades    u     ON u.IdUnidad           = m.IdUnidad;
+    INNER JOIN Comandas          c     ON c.IdComanda           = m.IdComanda
+    INNER JOIN Recetas           r     ON r.IdReceta            = c.IdReceta
+    INNER JOIN TiposModificacion tm    ON tm.IdTipoModificacion = m.IdTipoModificacion
+    LEFT  JOIN Ingredientes      iOrig ON iOrig.IdIngrediente   = m.IdIngredienteOriginal
+    LEFT  JOIN Ingredientes      iRemp ON iRemp.IdIngrediente   = m.IdIngredienteReemplazo
+    INNER JOIN Unidades          u     ON u.IdUnidad            = m.IdUnidad;
